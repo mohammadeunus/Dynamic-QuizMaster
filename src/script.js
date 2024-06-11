8b52d3cbe0b8eb05b780057b274395c3fb978545
mohammad eunus <100eunus@gmail.com>
2024-06-12 00:57:49 +0600

    dynamic working

--- src/script.js
+++ src/script.js
@@ -1,134 +1,71 @@
- 
 const singleSelectQuizTemplate = `
 <ul>
-<li>
-  <input type="radio" name="answer" id="a" class="answer">
-  <label for="a" id="a_text">Answer</label>
-</li>
-<li>
-  <input type="radio" name="answer" id="b" class="answer">
-  <label for="b" id="b_text">Answer</label>
-</li>
-<li>
-  <input type="radio" name="answer" id="c" class="answer">
-  <label for="c" id="c_text">Answer</label>
-</li>
-<li>
-  <input type="radio" name="answer" id="d" class="answer">
-  <label for="d" id="d_text">Answer</label>
-</li>
+  <li>
+    <input type="radio" name="answer" id="a" class="answer">
+    <label for="a" id="a_text">Answer</label>
+  </li>
+  <li>
+    <input type="radio" name="answer" id="b" class="answer">
+    <label for="b" id="b_text">Answer</label>
+  </li>
+  <li>
+    <input type="radio" name="answer" id="c" class="answer">
+    <label for="c" id="c_text">Answer</label>
+  </li>
+  <li>
+    <input type="radio" name="answer" id="d" class="answer">
+    <label for="d" id="d_text">Answer</label>
+  </li>
 </ul>
-`; 
+`;
 
 const multiSelectQuizTemplate = `
 <ul>
-<li>
-  <input type="checkbox" name="answer" id="a" class="answer">
-  <label for="a" id="a_text">Answer</label>
-</li>
-<li>
-  <input type="checkbox" name="answer" id="b" class="answer">
-  <label for="b" id="b_text">Answer</label>
-</li>
-<li>
-  <input type="checkbox" name="answer" id="c" class="answer">
-  <label for="c" id="c_text">Answer</label>
-</li>
-<li>
-  <input type="checkbox" name="answer" id="d" class="answer">
-  <label for="d" id="d_text">Answer</label>
-</li>
+  <li>
+    <input type="checkbox" name="answer" id="a" class="answer">
+    <label for="a" id="a_text">Answer</label>
+  </li>
+  <li>
+    <input type="checkbox" name="answer" id="b" class="answer">
+    <label for="b" id="b_text">Answer</label>
+  </li>
+  <li>
+    <input type="checkbox" name="answer" id="c" class="answer">
+    <label for="c" id="c_text">Answer</label>
+  </li>
+  <li>
+    <input type="checkbox" name="answer" id="d" class="answer">
+    <label for="d" id="d_text">Answer</label>
+  </li>
 </ul>
 `;
 
 const booleanQuestionTemplate = `
 <ul>
-
-<li>
-  <div class="row" style="display: inline-block;">
-  <div class="col-8">
-    <p>Is the sky blue?</p>
-  </div>
-  <div class="col-2">
-    <input type="radio" name="answer_a" id="a_true" class="answer" value="true">
+  <li>
+    <input type="radio" name="answer" id="a_true" class="answer" value="true">
     <label for="a_true" id="a_true_text">Yes</label>
-  </div>
-  <div class="col-2">
-    <input type="radio" name="answer_a" id="a_false" class="answer" value="false">
+    <input type="radio" name="answer" id="a_false" class="answer" value="false">
     <label for="a_false" id="a_false_text">No</label>
-  </div>
-  </div>
-</li>
-<!-- 
-
-<li style="display: inline-block;">
-  <input type="radio" name="answer_b" id="b_true" class="answer" value="true">
-  <label for="b_true" id="b_true_text">Yes</label>
-  <input type="radio" name="answer_b" id="b_false" class="answer" value="false">
-  <label for="b_false" id="b_false_text">No</label>
-</li>
-<li style="display: inline-block;">
-  <input type="radio" name="answer_c" id="c_true" class="answer" value="true">
-  <label for="c_true" id="c_true_text">Yes</label>
-  <input type="radio" name="answer_c" id="c_false" class="answer" value="false">
-  <label for="c_false" id="c_false_text">No</label>
-</li>
-<li style="display: inline-block;">
-  <input type="radio" name="answer_d" id="d_true" class="answer" value="true">
-  <label for="d_true" id="d_true_text">Yes</label>
-  <input type="radio" name="answer_d" id="d_false" class="answer" value="false">
-  <label for="d_false" id="d_false_text">No</label>
-</li> -->
+  </li>
 </ul>
 `;
 
-let currentQuiz = 0;
-let score = 0;
-
-
-
-debugger
 const quiz = document.getElementById("quiz");
+const quizSelection = document.getElementById("quiz-selection");
+const quizContent = document.getElementById("quiz-content");
 const quizBody = document.getElementById("quizBody");
-
-quizBody.innerHTML = multiSelectQuizTemplate;
-
-
-const answerEls = document.querySelectorAll(".answer");
 const questionEl = document.getElementById("question");
-const a_text = document.getElementById("a_text");
-const b_text = document.getElementById("b_text");
-const c_text = document.getElementById("c_text");
-const d_text = document.getElementById("d_text");
 const submitBtn = document.getElementById("submit");
+const previousBtn = document.getElementById("previous");
 const loadingProgress = document.getElementById("loading-bar-progress");
 
+let currentQuiz = 0;
+let score = 0;
+let selectedQuizType = '';
+let quizData = [];
 
-const booleanQuestionData = [
-  {
-    order: 1,
-    questionText: "Is the sky blue?",
-    correctAnswer: true
-  },
-  {
-    order: 2,
-    questionText: "Is water wet?",
-    correctAnswer: true
-  },
-  {
-    order: 3,
-    questionText: "Is the Earth flat?",
-    correctAnswer: false
-  },
-  {
-    order: 4,
-    questionText: "Is the Earth round?",
-    correctAnswer: true
-  }
-];
-
-
-const quizData = [
+const singleSelectQuizData = [
   {
     question: "Which language runs in a web browsers?",
     a: "Java",
@@ -150,7 +87,7 @@ const quizData = [
     a: "Hypertext Markup Language",
     b: "Hypertext Markdown Language",
     c: "Hyperloop Machine Language",
-    d: "Helicopters Terminals Motorboats Lamborginis",
+    d: "Helicopters Terminals Motorboats Lamborghinis",
     correct: "a"
   },
   {
@@ -163,50 +100,152 @@ const quizData = [
   }
 ];
 
+const multiSelectQuizData = [
+  {
+    question: "Select the programming languages.",
+    a: "JavaScript",
+    b: "BananaScript",
+    c: "Python",
+    d: "Ruby",
+    correct: ["a", "c", "d"]
+  },
+  {
+    question: "Select the CSS frameworks.",
+    a: "Bootstrap",
+    b: "Foundation",
+    c: "Angular",
+    d: "Bulma",
+    correct: ["a", "b", "d"]
+  },
+  {
+    question: "Select the HTML tags.",
+    a: "<div>",
+    b: "<banana>",
+    c: "<span>",
+    d: "<p>",
+    correct: ["a", "c", "d"]
+  },
+  {
+    question: "Select the JavaScript libraries.",
+    a: "React",
+    b: "Vue",
+    c: "Angular",
+    d: "JQuery",
+    correct: ["a", "b", "d"]
+  }
+];
 
-const resultTemplate = `
-      <h2>You answered ${score}/${quizData.length} questions correctly</h2>
+const booleanQuizData = [
+  {
+    question: "Is the sky blue?",
+    correct: true
+  },
+  {
+    question: "Is water wet?",
+    correct: true
+  },
+  {
+    question: "Is the Earth flat?",
+    correct: false
+  },
+  {
+    question: "Is the Earth round?",
+    correct: true
+  }
+];
 
-      <button onclick="location.reload()">Reload <i class="fa-solid fa-arrows-rotate"></i></button>
-  `;
+document.querySelectorAll(".quiz-type-btn").forEach(btn => {
+  btn.addEventListener("click", () => {
+    selectedQuizType = btn.getAttribute("data-type");
+    startQuiz();
+  });
+});
 
-loadQuiz();
+function startQuiz() {
+  quizSelection.style.display = "none";
+  quizContent.style.display = "block";
+  previousBtn.style.display = "none";
+  submitBtn.innerText = "Next";
+
+  switch (selectedQuizType) {
+    case "single":
+      quizData = singleSelectQuizData;
+      break;
+    case "multi":
+      quizData = multiSelectQuizData;
+      break;
+    case "boolean":
+      quizData = booleanQuizData;
+      break;
+  }
+
+  loadQuiz();
+}
 
 function loadQuiz() {
   deselectAnswers();
-
   const currentQuizData = quizData[currentQuiz];
 
   questionEl.innerText = currentQuizData.question;
-  a_text.innerText = currentQuizData.a;
-  b_text.innerText = currentQuizData.b;
-  c_text.innerText = currentQuizData.c;
-  d_text.innerText = currentQuizData.d;
+  switch (selectedQuizType) {
+    case "single":
+      quizBody.innerHTML = singleSelectQuizTemplate;
+      document.getElementById("a_text").innerText = currentQuizData.a;
+      document.getElementById("b_text").innerText = currentQuizData.b;
+      document.getElementById("c_text").innerText = currentQuizData.c;
+      document.getElementById("d_text").innerText = currentQuizData.d;
+      break;
+    case "multi":
+      quizBody.innerHTML = multiSelectQuizTemplate;
+      document.getElementById("a_text").innerText = currentQuizData.a;
+      document.getElementById("b_text").innerText = currentQuizData.b;
+      document.getElementById("c_text").innerText = currentQuizData.c;
+      document.getElementById("d_text").innerText = currentQuizData.d;
+      break;
+    case "boolean":
+      quizBody.innerHTML = booleanQuestionTemplate;
+      document.getElementById("a_true_text").innerText = "Yes";
+      document.getElementById("a_false_text").innerText = "No";
+      break;
+  }
 }
 
 function deselectAnswers() {
-  answerEls.forEach((answerEl) => (answerEl.checked = false));
+  const answerEls = document.querySelectorAll(".answer");
+  answerEls.forEach(answerEl => answerEl.checked = false);
 }
 
 function getSelected() {
-  let answer;
+  const answerEls = document.querySelectorAll(".answer");
+  let answers = [];
 
-  answerEls.forEach((answerEl) => {
+  answerEls.forEach(answerEl => {
     if (answerEl.checked) {
-      answer = answerEl.id;
+      answers.push(answerEl.id);
     }
   });
 
-  return answer;
+  return answers;
 }
 
 submitBtn.addEventListener("click", () => {
-	debugger
-  const answer = getSelected();
+  const answers = getSelected();
 
-  if (answer) {
-    if (answer === quizData[currentQuiz].correct) {
-      score++;
+  if (answers.length) {
+    const currentQuizData = quizData[currentQuiz];
+    if (selectedQuizType === "single") {
+      if (answers[0] === currentQuizData.correct) {
+        score++;
+      }
+    } else if (selectedQuizType === "multi") {
+      if (JSON.stringify(answers.sort()) === JSON.stringify(currentQuizData.correct.sort())) {
+        score++;
+      }
+    } else if (selectedQuizType === "boolean") {
+      const correctAnswer = currentQuizData.correct.toString();
+      if (answers[0] === `a_${correctAnswer}`) {
+        score++;
+      }
     }
 
     currentQuiz++;
@@ -215,11 +254,17 @@ submitBtn.addEventListener("click", () => {
     if (currentQuiz < quizData.length) {
       loadQuiz();
     } else {
-      quiz.innerHTML = resultTemplate
+      quiz.innerHTML = `
+        <h2>You answered ${score}/${quizData.length} questions correctly</h2>
+        <button onclick="location.reload()">Reload <i class="fa-solid fa-arrows-rotate"></i></button>
+      `;
     }
   }
 });
 
-
-
-
+previousBtn.addEventListener("click", () => {
+  if (currentQuiz > 0) {
+    currentQuiz--;
+    loadQuiz();
+  }
+});
